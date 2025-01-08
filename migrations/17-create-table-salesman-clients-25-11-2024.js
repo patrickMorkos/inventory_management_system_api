fd32fa411035f80cb0f5aa66fb52cc3b54465d3e
Patrick <patrick@tekwin.digital>
2025-01-08 21:39:17 +0200

    Update 17-create-table-salesman-clients-25-11-2024.js

--- migrations/17-create-table-salesman-clients-25-11-2024.js
+++ migrations/17-create-table-salesman-clients-25-11-2024.js
@@ -25,7 +25,7 @@ module.exports = {
                 type: Sequelize.INTEGER,
                 allowNull: false,
                 references: {
-                    model: 'client',
+                    model: 'Client',
                     key: 'id',
                 },
             },
