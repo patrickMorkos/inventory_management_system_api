8380df3f47445d7315b84eaf490af0f2eb6daf41
Patrick <patrick@tekwin.digital>
2025-01-30 19:20:01 +0200

    Update clientStockRoutes.js

--- routes/clientStockRoutes.js
+++ routes/clientStockRoutes.js
@@ -5,7 +5,7 @@ const authenticateToken = require('../lib/middlewares/middlewares');
 
 router.post('/add-products/:client_id', authenticateToken, clientStockController.addProductsToClientStock);
 router.get('/get-all-client-stock-products/:client_id', authenticateToken, clientStockController.getAllClientStocksProducts);
-router.put('/update-products-quantities/:client_id', authenticateToken, clientStockController.updateProductsQuantities);
+router.put('/update-products-quantities/:client_id', authenticateToken, clientStockController.updateProductQuantity);
 router.delete('/remove-product-from-client-stock/:client_id', authenticateToken, clientStockController.removeProductFromClientStock);
 
 module.exports = router;
